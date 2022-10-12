e9db81573775f6d36c8bdf6e36feb00a9c352dfe
harish <tharish0206@gmail.com>
2022-10-12 13:19:20 +0530

    Frist commit

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class loops 
+{
+    public static void main(String args[]) 
+    {
+        int num1=8;
+        int num2=7;
+
+
+        if(num1>num2)
+        {
+            
+         System.out.println(num1);
+        }
+        else
+        {
+            System.out.println(num2);
+        }
+
+        
+    }
+    
+}
